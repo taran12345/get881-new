9bc1c5227ba648e196f4cfcf1f1505dbae152f11
taran12345 <tarangangil26@gmail.com>
2020-12-18 16:28:20 +0530

    modified methods

--- DemoApp/src/model/Calc.java
+++ DemoApp/src/model/Calc.java
@@ -2,15 +2,15 @@ package model;
 
 public class Calc {
 	public void add(int n1, int n2) {
-		System.out.println("Sum= "+(n1+n2));
+		System.out.println("Sum of two no = "+(n1+n2));
 	}
 	public void sub(int n1, int n2) {
-		System.out.println("Difference= "+(n1-n2));
+		System.out.println("Difference of two no = "+(n1-n2));
 	} 
 	public void mul(int n1, int n2) {
-		System.out.println("Mulitply= "+(n1*n2));
+		System.out.println("Mulitply of two no = "+(n1*n2));
 	}
 	public void div(int n1, int n2) {
-		System.out.println("Divide= "+(n1/n2));
+		System.out.println("Divide of two no = "+(n1/n2));
 	}
 }
