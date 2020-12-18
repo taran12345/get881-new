e3e6c914f3408549feb623b2a231a7e2905c7b7f
Taran gangil <40990378+taran12345@users.noreply.github.com>
2020-12-18 16:35:24 +0530

    Update Calc.java

--- DemoApp/src/model/Calc.java
+++ DemoApp/src/model/Calc.java
@@ -2,7 +2,7 @@ package model;
 
 public class Calc {
 	public void add(int n1, int n2) {
-		System.out.println("Sum= "+(n1+n2));
+		System.out.println("Sum of 2 no= "+(n1+n2));
 	}
 	public void sub(int n1, int n2) {
 		System.out.println("Difference= "+(n1-n2));
